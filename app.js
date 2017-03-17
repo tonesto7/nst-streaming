954f7c0087502dc3df2fc93fdd692d5b178ce974
tonesto7 <tonesto7@gmail.com>
2017-03-17 16:58:41 -0400

    lot's of changes

--- app.js
+++ app.js
@@ -1,249 +1,341 @@
+/*jshint esversion: 6 */
 /*
 	NST-Streaming
-	Version 0.1.0
+	Version 0.2.0
 	Author: Anthony Santilli
 	Copyright 2017 Anthony Santilli
 
 	Big thanks to Greg Hesp (@ghesp) for portions of the code and your helpful ideas.
- */
+*/
 
-var codeVer = "0.1.0";
+var appVer = '0.2.0';
+const nest_api_url = 'https://developer-api.nest.com';
+const winston = require('winston');
+const fs = require('fs');
+
 var http = require('http');
 var request = require('request');
 var express = require('express');
-var moment = require('moment');
-var os = require("os");
+var os = require('os');
 var EventSource = require('eventsource');
 var app = express();
 
-var nest_api_url = 'https://developer-api.nest.com';
+const moment = require('moment');
+const tsFormat = () => getPrettyDt();
+const logDir = 'logs';
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+	fs.mkdirSync(logDir);
+}
+
 var source;
 var nestToken = null;
 var stToken = null;
 var callbackUrl = null;
 var requestStreamOn = false;
 var isStreaming = false;
-var serviceStartTime = Date.now();
+var serviceStartTime = Date.now(); //Returns time in millis
 var serviceStartDt = getDtNow();
 var lastEventDt = null;
 var lastEventData = null;
 
+// This initializes the winston logging instance
+var logger = new (winston.Logger)({
+	levels: {
+	    trace: 0,
+	    input: 1,
+	    verbose: 2,
+	    prompt: 3,
+	    debug: 4,
+	    info: 5,
+	    data: 6,
+	    help: 7,
+	    warn: 8,
+	    error: 9
+	},
+	colors: {
+	    trace: 'magenta',
+	    input: 'grey',
+	    verbose: 'cyan',
+	    prompt: 'grey',
+	    debug: 'yellow',
+	    info: 'green',
+	    data: 'blue',
+	    help: 'cyan',
+	    warn: 'orange',
+	    error: 'red'
+	},
+	transports: [
+		new (winston.transports.Console)({
+			levels: 'trace',
+			colorize: true,
+			prettyPrint: true,
+			timestamp: tsFormat
+		}),
+		new (require('winston-daily-rotate-file'))({
+			filename: `${logDir}/ - NST_Streaming_Service.log`,
+			levels: 'trace',
+			colorize: true,
+			prettyPrint: true,
+			timestamp: tsFormat,
+			json: false,
+			localTime: true,
+			datePattern: 'MM-dd-yyyy',
+			maxFiles: 20,
+			prepend: true
+		})
+	],
+	exitOnError: false
+});
+
 app.post('/stream', function(req, res) {
-    nestToken = req.headers.nesttoken;
-    callbackUrl = req.headers.callback;
-    requestStreamOn = req.headers.connstatus;
-    stToken = req.headers.sttoken;
-    //console.log(req.headers);
-    manageStream();
+	nestToken = req.headers.nesttoken;
+	callbackUrl = req.headers.callback;
+	requestStreamOn = req.headers.connstatus;
+	stToken = req.headers.sttoken;
+	//logger.debug(req.headers);
+	manageStream();
 });
 
 //Returns Status of Service
 app.post('/status', function(req, res) {
-    callbackUrl = req.headers.callback;
-    stToken = req.headers.sttoken;
-    console.log('[' + getPrettyDt() + ']: ', 'SmartThings is Requesting Status...');
+	callbackUrl = req.headers.callback;
+	stToken = req.headers.sttoken;
+	logger.info('SmartThings is Requesting Status...');
 
-    var statRequest = require('request');
-    if (callbackUrl && stToken) {
-        statRequest({
-            url: callbackUrl + '/streamStatus?access_token=' + stToken,
-            method: 'POST',
-            json: {
-                "streaming": isStreaming,
-                "version": codeVer,
-                "startupDt": getServiceUptime(),
-                "lastEvtDt": lastEventDt,
-                "hostInfo": getHostInfo()
-            }
-        }, function(error, response, body) {
-            if (error) {
-        	console.log("/status... ", error, response.statusCode, response.statusMessage);
-            } else {
-                //console.log("/status... ", response.statusCode, body);
-            }
-        });
-    }
+	var statRequest = require('request');
+	if (callbackUrl && stToken) {
+		statRequest({
+			url: callbackUrl + '/streamStatus?access_token=' + stToken,
+			method: 'POST',
+			json: {
+				'streaming': isStreaming,
+				'version': appVer,
+				'startupDt': getServiceUptime(),
+				'lastEvtDt': lastEventDt,
+				'hostInfo': getHostInfo()
+			}
+		}, function(error, response, body) {
+			if (error) {
+				   logger.error('/status error... ', error, response.statusCode, response.statusMessage);
+			} else {
+				//logger.debug('/status... ', response.statusCode, body);
+			}
+		});
+	} else {
+		logger.warn('/status: Can\'t send Status back to SmartThings because the enpoint url or access token are missing...');
+	}
 });
 
 function manageStream() {
-    if (isStreaming && requestStreamOn == 'false') {
-        source.close();
-	lastEventData = null;
-	isStreaming = false;
-        sendStatusToST("ManagerClosed");
-        console.log('[' + getPrettyDt() + ']: ', "Streaming Connection has been Closed");
-    } else if (!isStreaming && requestStreamOn == 'true') {
-        startStreaming();
-        isStreaming = true;
-    } else {
-        isStreaming = false;
-    }
+	if (isStreaming && requestStreamOn == 'false') {
+		source.close();
+		lastEventData = null;
+		isStreaming = false;
+		sendStatusToST('ManagerClosed');
+		logger.warn('Streaming Connection has been Closed');
+	} else if (!isStreaming && requestStreamOn == 'true') {
+		startStreaming();
+		isStreaming = true;
+	} else {
+		isStreaming = false;
+	}
 }
 
 function startStreaming() {
-    //console.log("Start Stream");
-    source = new EventSource(nest_api_url + '?auth=' + nestToken);
-    source.addEventListener('put', function(e) {
-        var data = e.data;
-        //console.log(data);
-        console.log('[' + getPrettyDt() + ']: ', 'New Event Data Received...');
-        if (data && lastEventData != data) {
-            lastEventDt = getDtNow();
-            lastEventData = data;
-            console.log('[' + getPrettyDt() + ']: ', "Data sent to ST");
-            if (sendDataToST(data)) {
+	//logger.debug("Start Stream");
+	source = new EventSource(nest_api_url + '?auth=' + nestToken);
+	source.addEventListener('put', function(e) {
+		var data = e.data;
+		//logger.debug(data);
+		logger.info('New Nest API Event Data Received...');
+		if (data && lastEventData != data) {
+			lastEventDt = getDtNow();
+			lastEventData = data;
+			logger.info("Sent Nest Event Data to NST Manager Client (ST)");
+			if (sendDataToST(data) === true) {
 // this is never reached...
-                isStreaming = true;
-            }
-        }
-    });
-    source.addEventListener('open', function(e) {
-        console.log('[' + getPrettyDt() + ']: ', 'Nest Connection Opened!');
-        isStreaming = true;
-    });
-    source.addEventListener('auth_revoked', function(e) {
-        console.log('Stream Authentication token was revoked.');
-        source.close();
-	isStreaming = false;
-	lastEventData = null;
-        sendStatusToST("ManagerClosed");
-    });
-    source.addEventListener('error', function(e) {
-        if (e.readyState == EventSource.CLOSED) {
-            console.error('[' + getPrettyDt() + ']: ', 'Stream Connection was closed! ', e);
-        } else {
-            console.error('[' + getPrettyDt() + ']: ', 'A Stream unknown error occurred: ', e);
-        }
-        source.close();
-	isStreaming = false;
-	lastEventData = null;
-        sendStatusToST("ManagerClosed");
-    }, false);
+				isStreaming = true;
+			}
+		}
+	});
+	source.addEventListener('open', function(e) {
+		logger.info('Nest Connection Opened!');
+		isStreaming = true;
+	});
+	source.addEventListener('auth_revoked', function(e) {
+		logger.warn('Stream Authentication token was revoked.');
+		source.close();
+		isStreaming = false;
+		lastEventData = null;
+		sendStatusToST("ManagerClosed");
+	});
+	source.addEventListener('error', function(e) {
+		if (e.readyState == EventSource.CLOSED) {
+			logger.warn('Stream Connection was closed! ', e);
+		} else {
+			logger.error('A Stream unknown error occurred: ', e);
+		}
+		source.close();
+		isStreaming = false;
+		lastEventData = null;
+		sendStatusToST("ManagerClosed");
+	}, false);
 }
 
 function sendDataToST(data) {
-    var options = {
-        uri: callbackUrl + '/receiveEventData?access_token=' + stToken,
-        method: 'POST',
-        body: data
-    };
-    request(options, function(error, response, body) {
-        if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
-            //console.log("sendDataToST body.id... ", body.id);
-            return true;
-        } else {
-            console.log("sendDataToST... ", error, response.statusCode, response.statusMessage);
-	    lastEventData = null;
-            return false;
-        }
-    });
+	var options = {
+		uri: callbackUrl + '/receiveEventData?access_token=' + stToken,
+		method: 'POST',
+		body: data
+	};
+	request(options, function(error, response, body) {
+		if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
+			//logger.debug("sendDataToST body.id... ", body.id);
+			return true;
+		} else {
+			logger.error('sendDataToST...error ', error, response.statusCode, response.statusMessage);
+		lastEventData = null;
+			return false;
+		}
+	});
 }
 
 function sendStatusToST(reason) {
-    var request2 = require('request');
-    if (callbackUrl && stToken) {
-        var options = {
-            uri: callbackUrl + '/streamStatus?access_token=' + stToken,
-            method: 'POST',
-            json: {
-	        "streaming": isStreaming,
-                "version": codeVer,
-                "startupDt": getServiceUptime(),
-                "lastEvtDt": lastEventDt,
-                "hostInfo": getHostInfo(),
-	        "exitReason": reason
-            }
-        };
-        console.log("url and token found");
-        request2(options, function(error, response, body) {
-            if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
-                //console.log("sendStatusToST...body ", body.id);
-                return true;
-            } else {
-            	console.log("sendStatusToST...error ", error, response.statusCode, response.statusMessage);
-                return false;
-            }
-        });
-    } else {
-        console.log("sendStatusToST: Can't send status because url or token missing...");
-    }
+	var request2 = require('request');
+	if (callbackUrl && stToken) {
+		var options = {
+			uri: callbackUrl + '/streamStatus?access_token=' + stToken,
+			method: 'POST',
+			json: {
+				'streaming': isStreaming,
+				'version': appVer,
+				'startupDt': getServiceUptime(),
+				'lastEvtDt': lastEventDt,
+				'hostInfo': getHostInfo(),
+			    'exitReason': reason
+			}
+		};
+		logger.debug('url and token found');
+		request2(options, function(error, response, body) {
+			if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
+				//logger.debug("sendStatusToST...body ", body.id);
+				return true;
+			} else {
+				logger.error('sendStatusToST...error', error, response.statusCode, response.statusMessage);
+				return false;
+			}
+		});
+	} else {
+		logger.warn('sendStatusToST: Can\'t send Status back to SmartThings because the enpoint url or access token are missing...');
+	}
 }
 
 function getIPAddress() {
-    var interfaces = os.networkInterfaces();
-    for (var devName in interfaces) {
-        var iface = interfaces[devName];
-        for (var i = 0; i < iface.length; i++) {
-            var alias = iface[i];
-            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
-                return alias.address;
-        }
-    }
-    return '0.0.0.0';
+	var interfaces = os.networkInterfaces();
+	for (var devName in interfaces) {
+		var iface = interfaces[devName];
+		for (var i = 0; i < iface.length; i++) {
+			var alias = iface[i];
+			if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+				return alias.address;
+		}
+	}
+	return '0.0.0.0';
 }
 
 function getServiceUptime() {
-    var now = Date.now();
-    var diff = (now - serviceStartTime) / 1000;
-    //console.log("diff: "+ diff);
-    return getHostUptimeStr(diff);
+	var now = Date.now();
+	var diff = (now - serviceStartTime) / 1000;
+	//logger.debug("diff: "+ diff);
+	return getHostUptimeStr(diff);
 }
 
 function getHostInfo() {
-    var hostName = os.hostname();
-    var osType = os.type();
-    var osArch = os.arch();
-    var osRelease = os.release();
-    var osPlatform = os.platform();
-    var osUptime = os.uptime();
-    var memTotal = os.totalmem();
-    var memFree = os.freemem();
+	var hostName = os.hostname();
+	var osType = os.type();
+	var osArch = os.arch();
+	var osRelease = os.release();
+	var osPlatform = os.platform();
+	var osUptime = parseInt(os.uptime());
+	var memTotal = os.totalmem();
+	var memFree = os.freemem();
 
-    if (osType != "Windows_NT") {
-        osPlatform = getLinuxPlatform();
-    }
-    return { "hostname": hostName, "osType": osType, "osArch": osArch, "osRelease": osRelease, "osPlatform": osPlatform, "osUptime": getHostUptimeStr(osUptime), "memTotal": formatBytes(memTotal), "memFree": formatBytes(memFree) };
+	if (osType != 'Windows_NT') {
+		osPlatform = getLinuxPlatform();
+	}
+	return {
+			'hostname': hostName,
+			'osType': osType,
+			'osArch': osArch,
+			'osRelease': osRelease,
+			'osPlatform': osPlatform,
+			'osUptime': getHostUptimeStr(osUptime),
+			'osUptimeStr': secondsToText(osUptime),
+			'memTotal': formatBytes(memTotal),
+			'memFree': formatBytes(memFree)
+	};
 }
 
 function getLinuxPlatform() {
-    var osval;
-    var getos = require('getos');
-    getos(function(e, os) {
-        if (e) {
-            return console.log(e);
-        } else {
-            osval = JSON.stringify(os);
-        }
-    });
-    return osval;
+	var osval;
+	var getos = require('getos');
+	getos(function(e, os) {
+		if (e) {
+			return logger.error(e);
+		} else {
+			osval = JSON.stringify(os);
+		}
+	});
+	return osval;
 }
 
 function getDtNow() {
-    var date = new Date();
-    return date.toISOString();
+	var date = new Date();
+	return date.toISOString();
 }
 
 function getPrettyDt() {
-    if (moment) {
-        return moment().format('MMMM Do YYYY, h:mm:ssa');
-    }
+	if (moment) {
+		return moment().format('M-D-YYYY - h:mm:ssa');
+	}
 }
 
 function getHostUptimeStr(time) {
-    var hours = Math.floor(time / 3600);
-    time -= hours * 3600;
-    var minutes = Math.floor(time / 60);
-    time -= minutes * 60;
-    var seconds = parseInt(time % 60, 10);
-    //console.log(hours + ' Hours - ' + minutes + ' Minutes and ' + seconds + " Seconds");
-    return (hours + ' Hrs - ' + minutes + ' Min and ' + seconds + " Sec");
+	var years = Math.floor(time / 31536000);
+	time -= years * 31536000;
+	var days = Math.floor(time / 86400);
+	time -= days * 86400;
+	var hours = Math.floor(time / 3600);
+	time -= hours * 3600;
+	var minutes = Math.floor(time / 60);
+	time -= minutes * 60;
+	var seconds = parseInt(time % 60, 10);
+	return (years + 'y, ' + days + 'd, ' + hours + 'h:' + (minutes < 10 ? '0' + minutes : minutes) + 'm:' + (seconds < 10 ? '0' + seconds : seconds) +'s');
+}
+
+function secondsToText(seconds) {
+    var levels = [
+        [Math.floor(seconds / 31536000), 'years'],
+        [Math.floor((seconds % 31536000) / 86400), 'days'],
+        [Math.floor(((seconds % 31536000) % 86400) / 3600), 'hours'],
+        [Math.floor((((seconds % 31536000) % 86400) % 3600) / 60), 'minutes'],
+        [(((seconds % 31536000) % 86400) % 3600) % 60, 'seconds'],
+    ];
+    var returntext = '';
+
+    for (var i = 0, max = levels.length; i < max; i++) {
+        if ( levels[i][0] === 0 ) continue;
+        returntext += ' ' + levels[i][0] + ' ' + (levels[i][0] === 1 ? levels[i][1].substr(0, levels[i][1].length-1): levels[i][1]);
+    }
+    return returntext.trim();
 }
 
 function formatBytes(bytes) {
-    if (bytes < 1024) return bytes + " Bytes";
-    else if (bytes < 1048576) return (bytes / 1024).toFixed(1) + " KB";
-    else if (bytes < 1073741824) return (bytes / 1048576).toFixed(1) + " MB";
-    else return (bytes / 1073741824).toFixed(1) + " GB";
+	if (bytes < 1024) return bytes + " Bytes";
+	else if (bytes < 1048576) return (bytes / 1024).toFixed(1) + " KB";
+	else if (bytes < 1073741824) return (bytes / 1048576).toFixed(1) + " MB";
+	else return (bytes / 1073741824).toFixed(1) + " GB";
 }
 
 var hostAddr = getIPAddress();
@@ -253,21 +345,23 @@ app.set('port', port);
 
 var server = http.createServer(app);
 server.listen(port);
-console.info('[' + getPrettyDt() + ']: NST Stream Service (v' + codeVer + ') is Running at (IP: ' + hostAddr + ' | Port: ' + port + ')');
-console.info('Waiting for NST Manager client to send the required data in order to initialize the Nest Event Stream');
+logger.info('NST Stream Service (v' + appVer + ') is Running at (IP: ' + hostAddr + ' | Port: ' + port + ')');
+logger.info('Waiting for NST Manager to send this service the signal to initialize the Nest Event Stream...');
 
 
 process.stdin.resume(); //so the program will not close instantly
 
 function exitHandler(options, err) {
-    isStreaming = false;
-    if (options.cleanup) {
-        sendStatusToST("ClosedByUserConsole");
-    }
-    if (err) {
-        sendStatusToST("ClosedByError");
-    }
-    if (options.exit) process.exit();
+	isStreaming = false;
+	if (options.cleanup) {
+		logger.debug('exitHandler: ', 'ClosedByUserConsole');
+		sendStatusToST('ClosedByUserConsole');
+	}
+	if (err) {
+		logger.error('exitHandler error', err);
+		sendStatusToST('ClosedByError');
+	}
+	if (options.exit) process.exit();
 }
 
 //do something when app is closing
--- server.js
+++ server.js
@@ -1,13 +1,91 @@
+/*jshint esversion: 6 */
+/*
+	NST-Streaming
+	Version 0.2.0
+	Author: Anthony Santilli
+	Copyright 2017 Anthony Santilli
+
+	Big thanks to Greg Hesp (@ghesp) for portions of the code and your helpful ideas.
+ */
+
+const winston = require('winston');
+const fs = require('fs');
+const moment = require('moment');
+const tsFormat = () => getPrettyDt();
+const logDir = 'logs';
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+	fs.mkdirSync(logDir);
+}
+
+// This initializes the winston logging instance
+var logger = new (winston.Logger)({
+	levels: {
+	    trace: 0,
+	    input: 1,
+	    verbose: 2,
+	    prompt: 3,
+	    debug: 4,
+	    info: 5,
+	    data: 6,
+	    help: 7,
+	    warn: 8,
+	    error: 9
+	},
+	colors: {
+	    trace: 'magenta',
+	    input: 'grey',
+	    verbose: 'cyan',
+	    prompt: 'grey',
+	    debug: 'yellow',
+	    info: 'green',
+	    data: 'blue',
+	    help: 'cyan',
+	    warn: 'orange',
+	    error: 'red'
+	},
+	transports: [
+		new (winston.transports.Console)({
+			levels: 'trace',
+			colorize: true,
+			prettyPrint: true,
+			timestamp: tsFormat
+		}),
+		new (require('winston-daily-rotate-file'))({
+			filename: `${logDir}/ - NST_Monitor_Service.log`,
+			levels: 'trace',
+			colorize: true,
+			prettyPrint: true,
+			timestamp: tsFormat,
+			json: false,
+			localTime: true,
+			datePattern: 'MM-dd-yyyy',
+			maxFiles: 20,
+			prepend: true
+		})
+	],
+	exitOnError: false
+});
+
+function getPrettyDt() {
+	if (moment) {
+		return moment().format('M-D-YYYY - h:mm:ssa');
+	}
+}
+
 // Beginning of NodeMon Code
 var nodemon = require('nodemon');
 nodemon({
-    script: 'app.js'
+	script: 'app.js'
 });
 
 nodemon.on('start', function() {
-    console.log('NST Streaming Service has Started');
+	console.log('\x1Bc');
+	logger.info('NST Service Monitor has Started');
 }).on('quit', function() {
-    console.log('NST Streaming Service has Quit.  Restarting...');
+	console.log('\x1Bc');
+	logger.info('NST Streaming Service has Quit. Restarting...');
 }).on('restart', function(files) {
-    console.log('NST Service Restarted because the following file was Updated: ', files);
+	console.log('\x1Bc');
+	logger.info('NST Service has been restarted because this file was Updated: ', files);
 });
