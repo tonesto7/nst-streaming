a7c64a0d9b3f1fbde3a63ebe07b158fdee86cf14
imnotbob <Imnot_bob@hotmail.com>
2017-03-27 14:53:09 -0400

    Cleanup

--- nst_update.sh
+++ nst_update.sh
@@ -1,15 +1,103 @@
 #!/bin/bash
 
-srvc_name="nst-streaming.service"
-old_srvc="/etc/systemd/system/nst-streaming.service"
-new_srvc="/home/pi/nst-streaming-master/nst-streaming.service"
-zip_file="/home/pi/nst-streaming-master.zip"
-app_dir="/home/pi/nst-streaming-master"
+THEUSER=pi
+local_dir="/home/$THEUSER"
+localapp_dir="$local_dir/nst-streaming-master"
 
-check_for_update() {
+zip_name="nst-streaming-master.zip"
+local_file="$local_dir/$zip_name"
+
+srvc_name="nst-streaming.service"
+new_srvcinstall="/etc/systemd/system/$srvc_name"
+new_srvcfile="$localapp_dir/$srvc_name"
+
+old_name="nst-streaming.service"
+old_srvcinstall="/etc/systemd/system/$old_name"
+
+remote_file="https://dl.dropboxusercontent.com/s/axr6bi9g73di5px/$zip_name"
+
+
+download_install_zip() {
+    echo "Downloading $zip_name..."
+    wget -N $remote_file -P $local_dir
+
+    if [ -f $local_file ];
+    then
+        stop_old_srvc
+
+        cd $local_dir
+        unzip -o $local_file
+        cd $localapp_dir
+        npm install --no-optional
+
+        if [ -f $new_srvcfile ];
+        then
+            echo "New NST Streaming Service File found. Installing!!!"
+            update_srvc
+        else
+            echo "New NST Service file not present..."
+            exit 1
+        fi
+
+    else
+        echo "Zip file not present..."
+        exit 1
+    fi
+}
+
+stop_old_srvc() {
+    if [ -f $old_srvcinstall ];
+    then
+        echo "Existing NST Streaming Service File found"
+#   Always restart if new zip file is given
+#        if [[ ! $old_srvcinstall -ef $new_srvcfile ]];
+#        then
+            echo "Removing Old Service"
+            remove_srvc
+#        else
+#            echo "Existing NST Streaming Service File is same as downloaded version"
+#        fi
+    else
+        echo "Existing NST Streaming Service not present..."
+    fi
+}
+
+remove_srvc() {
+    echo "Disabling and Removing existing NST Streaming Service File"
+    sudo systemctl stop $old_name
+    sudo systemctl disable $old_name
+    sudo rm $old_srvcinstall
+}
+
+update_srvc() {
+    if [ ! -f "$old_srvcinstall" ];
+    then
+        echo "Copying Updated NST Streaming Service File to Systemd Folder"
+        sudo cp $new_srvcfile $new_srvcinstall -f
+
+        echo "Reloading Systemd Daemon to reflect service changes..."
+        sudo systemctl daemon-reload
+        echo "Enabling NST Service..."
+        sudo systemctl enable $srvc_name
+        echo "Starting up NST Streaming Service..."
+        sudo systemctl start $srvc_name
+    else
+        echo "Not copying new service as old file is still there..."
+        exit 1
+    fi
+}
+
+
+echo "$0  $1"
+if [ "$1" = "-f" ];
+then
+    echo "Removing $local_file ..."
+    rm -rf $local_file
+fi
+
+if [ -f $local_file ];
+then
     echo "Checking for Newer file on remote server..."
-    remote_file="https://dl.dropboxusercontent.com/s/axr6bi9g73di5px/nst-streaming-master.zip"
-    local_file="/home/pi/nst-streaming-master.zip"
     modified=$(curl --silent --head $remote_file |
                awk -F: '/^Last-Modified/ { print $2 }')
     remote_ctime=$(date --date="$modified" +%s)
@@ -19,70 +107,11 @@ check_for_update() {
     echo "remote file time: $remote_ctime"
     if [ $local_ctime -lt $remote_ctime ];
     then
-        download_zip
+        echo "Updating..."
     else
         echo "Your version is the current...Skipping..."
+        exit
     fi
-}
-
-download_zip() {
-    echo "Downloading nst-streaming-master.zip..."
-    sudo wget -N https://dl.dropboxusercontent.com/s/axr6bi9g73di5px/nst-streaming-master.zip -P /home/pi
-    cd /home/pi
-    sudo unzip -o nst-streaming-master.zip
-    cd /home/pi/nst-streaming-master
-    sudo npm install
-    check_srvc_file
-}
-
-check_srvc_file() {
-    if [ -d $app_dir ];
-    then
-
-        if [ -f $old_srvc ];
-        then
-            echo "Existing NST Streaming Service File found"
-#   Always restart if new zip file is given
-#            if [[ ! $old_srvc -ef $new_srvc ]];
-#            then
-                echo "New NST Streaming Service File found. Removing Old Service and Updating!!!"
-                remove_srvc
-                update_srvc
-#            else
-#                echo "Existing NST Streaming Service File is same as downloaded version"
-#            fi
-        else
-            echo "Existing NST Streaming Service is Missing..."
-            update_srvc
-        fi
-    fi
-}
-
-remove_srvc() {
-    echo "Disabling and Removing existing NST Streaming Service File"
-    sudo systemctl stop $srvc_name
-    sudo systemctl disable $srvc_name
-    sudo rm $old_srvc
-}
-
-update_srvc() {
-    if [ ! -f "$old_srvc" ];
-    then
-        echo "Copying Updated NST Streaming Service File to Systemd Folder"
-        sudo cp $new_srvc $old_srvc -f
-
-        echo "Reloading Systemd Daemon to reflect service changes..."
-        sudo systemctl daemon-reload
-        echo "Enabling NST Service..."
-        sudo systemctl enable $srvc_name
-        echo "Starting up NST Streaming Service..."
-        sudo systemctl start $srvc_name
-    fi
-}
-
-if [ -f "$zip_file" ] || [ ! -f "$old_srvc"];
-then
-    check_for_update
-else
-    download_zip
 fi
+
+download_install_zip
